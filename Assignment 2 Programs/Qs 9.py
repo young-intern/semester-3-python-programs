caafbb1bfb0d6c4968c8f5690bd9f47361ef051b
Tapoban Ray <raytapoban@gmail.com>
2024-09-11 22:21:12 +0530

    Edited one program

--- Assignment 2 Programs/Qs 9.py
+++ Assignment 2 Programs/Qs 9.py
@@ -9,7 +9,7 @@ isPrime = True
 
 print(f"Among the first {n} numbers, these are the prime numbers: ", end=" ")
 
-for i in range(1, n):
+for i in range(1, n + 1):
     for j in range(2, round(math.sqrt(i)) + 1):
         if i % j == 0:
             break
