eab51b9a2256b35f4d679143c9dc05c73ca43f73
Tapoban Ray <raytapoban@gmail.com>
2024-09-11 22:20:31 +0530

    Edited one program

--- Assignment 2 Programs/Qs 9.py
+++ Assignment 2 Programs/Qs 9.py
@@ -7,10 +7,12 @@ n = int(input("Enter how many prime numbers you want to generate: "))
 prime_numbers = []
 isPrime = True
 
-print(f"Among the first {n} numbers, these are the prime numbers: ", 1, 2, end=" ")
-for i in range(3, n):
+print(f"Among the first {n} numbers, these are the prime numbers: ", end=" ")
+
+for i in range(1, n):
     for j in range(2, round(math.sqrt(i)) + 1):
         if i % j == 0:
             break
     else:
         print(i, end=" ")
+
